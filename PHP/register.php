b818af865e4f57fb3b45a413cb8f771431422062
Vial Amaury <amaury.vial503@gmail.com>
2022-10-27 11:52:06 +0200

    PHP V1

--- PHP/register.php
+++ PHP/register.php
@@ -1,25 +1,27 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "sae_s3";
+date_default_timezone_set('Europe/Paris');
+
+$host = "lucky.db.elephantsql.com";
+$user = "xpirrwid";
+$pass = "LkhxflJA_GDQQI_nqpkJBIbFBc955fiL";
+$db = "xpirrwid";
 
 
 try {
     
     //connection to database
-    $conn = new mysqli($servername, $username, $password,$db); 
-    if ($conn -> connect_error){
-        die("Connection failed : " . mysqli_connect_error());
-    }
+    $con = new PDO("pgsql:host=$host, dbname=$db, user=$user, password=$pass")
+    or die ("Could not connect to server\n");
+
+
 
     $pseudo =$_POST["pseudo"];
     $adr =$_POST["adr"];
     $password =$_POST["password"];
 
     $sqlCheckUser = "SELECT mail, pseudo FROM user";
-    $resultCheckUser = $conn->query($sqlCheckUser);
+    $resultCheckUser = $con->query($sqlCheckUser);
 
     $userExist = false;
 
@@ -36,20 +38,21 @@ try {
         $sqlNewUser = "INSERT INTO user (mail, pseudo, mot_de_passe)
         VALUES ('$adr', '$pseudo', '$password')";
 
-        if ($conn->query($sqlNewUser) === TRUE) {
+        if ($con->query($sqlNewUser) === TRUE) {
             echo "Compte crée";
             echo "lien pour telecharger le jeu";
         } else {
-            echo "Error: " . $sqlNewUser . "<br>" . $conn->error;
+            echo "Error: " . $sqlNewUser . "<br>" . $con->error;
         }
     }else{
         echo "mail ou pseudo deja utilisé";
     }
     
-    $conn -> close();
+    $cnn -> close();
 }
 
 catch(PDOException $e){
     echo $e->getMessage();
     }
+    
 ?>
