f9569369c40ead51e773280de17f6e8fc0dcd2c1
Amaury Vial <amaury.vial@etu.univ-amu.fr>
2022-11-29 21:38:23 +0100

    Ajout de la fonction mot de passe oublié avec l'envoie de mail fonctionnel

--- PHP/mdp_oublie.php
+++ PHP/mdp_oublie.php
@@ -1,14 +1,48 @@
 <?php
 
-$to = "findthebreach.noreply@gmail.com";
-$subject = "Utilisation mail avec php";
-$message = "Salut, ça va ";
+//Include required PHPMailer files
+require '../phpmailer/includes/Exception.php';
+require '../phpmailer/includes/SMTP.php';
+require '../phpmailer/includes/PHPMailer.php';
+//Define name spaces
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$headers = "Content-Type: text/plain; charset-utf-8\r\n";
-$headers = "From: findthebreach.noreply@gmail.com\r\n";
+//Create instance of PHPMailer
+$mail = new PHPMailer();
+//Set mailer to use smtp
+$mail->isSMTP();
+//Define smtp host
+$mail->Host = "smtp.gmail.com";
+//Enable smtp authentication
+$mail->SMTPAuth = true;
+//Set smtp encryption type (ssl/tls)
+$mail->SMTPSecure = "tls";
+//Port to connect smtp
+$mail->Port = "587";
+//Set gmail username
+$mail->Username = "findthebreach.noreply@gmail.com";
+//Set gmail password
+$mail->Password = "bpghsngrhhjqnznl";
+//Email subject
+$mail->Subject = "Test email using PHPMailer";
+//Set sender email
+$mail->setFrom("findthebreach.noreply@gmail.com");
+//Enable HTML
+$mail->isHTML(true);
 
-
-if (mail($to,$subject,$message,$headers))
-    echo 'Envoye ! ';
-else
-    echo 'Erreur envoi';
+//Email body
+$mail->Body = "VOICI UN MAIL !!";
+//Add recipient
+$mail_dest = $_POST["mail"];
+$mail->addAddress($mail_dest);
+//Finally send email
+if ( $mail->send() ) {
+    echo "Envoyé !";
+}else{
+    echo "Problème d'envoie";
+}
+//Closing smtp connection
+$mail->smtpClose();
+?>
