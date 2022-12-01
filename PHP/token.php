119949ee02c507aa6231c2023858f5b557fad722
Saadi Nils <91779594+18Nilsou@users.noreply.github.com>
2022-12-01 11:50:40 +0100

    Add files via upload

--- PHP/token.php
+++ PHP/token.php
@@ -1,22 +1,29 @@
 <?php
 require ("bdcon.php");
 
-
 function nouveauMdp($mdp, $email){
-
-    $sqlNickname = "select nickname from users where email = $email";
+    require ("bdcon.php");
+    $sqlNickname = "select nickname from users where email = '$email'";
     $sth = $con->prepare($sqlNickname);
     $sth -> execute();
     $nickname = $sth -> fetch();
     $nickname = $nickname['nickname'];
 
-    $mdp = hash("sha256", $mdp);
-    $sql = "update users where email=$email and nickname=$nickname set password = $mdp";
+    $mdp = hash("sha256", $mdp.$nickname);
+    $sql = "update users set password = '$mdp' where email='$email' and nickname='$nickname'";
     $sth = $con->prepare($sql);
     $sth -> execute();
-    
+    supprToken($mail);    
 }
 
+function supprToken($email){
+    require ("bdcon.php");
+    $sql = "DELETE FROM recup_mdp WHERE email='$email'";
+    $sth = $con->prepare($sql);
+    $sth -> execute();
+    header("location: ../index.html");
+    exit;
+}
 
 $mail = $_POST['mail'];
 $token = $_POST['token'];
@@ -29,13 +36,13 @@ while($row = $sth -> fetch()){
     if($row['token'] == $token){
         if($_POST['mdp'] ==  $_POST['mdpConfirmer']){
             nouveauMdp($_POST['mdp'], $mail);
-            header("location: ../index.html");
-            exit;
+            echo('azeazer');
+            
         }else{
             echo('mdp faux');
         }       
     }
-}else{
-        echo("token invalide");
-    }
+}
+
+echo("token invalide");
 ?>
