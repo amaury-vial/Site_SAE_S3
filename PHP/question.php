0e9cc2a3610c02e80af95ccc1ac3d2cb29d82400
Saadi Nils <91779594+18Nilsou@users.noreply.github.com>
2022-12-03 00:11:14 +0100

    Add files via upload

--- PHP/question.php
+++ PHP/question.php
@@ -3,11 +3,12 @@
 require ("bdcon.php");// on require la page pour ce connecter a la bd 
 
 //recup des variable du formulaire
-$idQues =$_POST["numque"];
-$question =$_POST["question"];
-$reponse =$_POST["réponse"];
-$indice =$_POST["Indice"];
-$consigne =$_POST["Consigne"];
+$idQues = str_replace('\'','\'\'',$_POST["numque"]);
+$question = str_replace('\'','\'\'',$_POST["question"]);
+$reponse = str_replace('\'','\'\'',$_POST["réponse"]);
+$indice = str_replace('\'','\'\'',$_POST["Indice"]);
+$consigne = str_replace('\'','\'\'',$_POST["Consigne"]);
+
 
 //requete pour modifier les questions 
 $sqlModificationQuestion = "UPDATE Question SET TXT='$consigne', TITLE='$question', SUGGESTION='$indice', SOLUTION='$reponse' WHERE Q_ID = $idQues";
