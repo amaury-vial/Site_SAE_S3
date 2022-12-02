17adaa5aee506f438b4b98f518f8e909e4fbffe2
Amaury Vial <82141847+amaury-vial@users.noreply.github.com>
2022-12-02 10:15:06 +0100

    Add files via upload
    Refonte Design FindTheBreach

--- PHP/pageAdmin.php
+++ PHP/pageAdmin.php
@@ -1,17 +1,5 @@
 <?php 
-function classement(){
-    require ("bdcon.php");// on require la page pour ce connecter a la bd
-    $classement = "<br><br><br>";
-        $sqlClassement = "Select nickname, score FROM USERS where score is not null order by score DESC limit 5";// requete pour recuperer le classement 
-        $sth = $con->prepare($sqlClassement);
-        $sth->execute();
-
-        //on affichage le classement
-        while($row = $sth->fetch()){
-            $classement = $classement.$row["nickname"]." : ".$row["score"]."<br>";
-        }
-        return $classement;
-}
+require ("bdcon.php");// on require la page pour ce connecter a la bd 
 
 ?>
 
@@ -27,45 +15,61 @@ function classement(){
 
 <body>
 
-    <!-- HEADER !-->
+<!-- HEADER !-->
 
-    <header>
-        <a href="../index.html">
-            <img class="Logo" src="../IMAGE/FindTheReach.png">
+<header>
+    <a href="../index.html">
+        <img class="Logo" src="../IMAGE/FindTheReach.png">
+    </a>
+
+    <h1 class="Link">
+        <a class="Title" href="../index.html">
+            <p>Find The Breach</p>    
         </a>
+    </h1>
+</header>
 
-        <h1 class="Link">
-            <a class="Title" href="../index.html">
-                <p>Find The Breach</p>    
-            </a>
-        </h1>
-    </header>
+<section class="corp">
+    <form method="post" action="question.php">
+        <h1><p>Modifier une question :</p></h1>
+        <div>
+        <label><p>No Question :</p></label><input type="number", name="numque" max="10" min="1" size="1em" required="required"/>
+        </div>
+        <label><p>Question :</p></label><input type="text" name="question" required="required"/>
+        <p>Consigne</p>
+        <textarea name="Consigne" rows="5" cols="80"  required="required" aria-required="true" style="height: 30%; width: 100%;" ></textarea>
+        <label><p>Réponse :</p></label><input type="text" name="réponse" required="required"/>
+        <label><p>Indice :</p></label><input type="text" name="Indice" required="required"/>
+        <br/>
+        <div><input type="submit" value="Valider"></div>
+    </form>
+    <section class="LeaderBord">
+        <h1><p>LeaderBoard</p></h1>
+        <div class="Player">
+            <?php 
+            echo("<br />");
+            echo("<br />");
+            echo("<br />");
+                    
+                    $sqlClassement = "Select nickname, score FROM USERS where score is not null order by score DESC limit 5";// requete pour recuperer le classement 
+                    $sth = $con->prepare($sqlClassement);
+                    $sth->execute();
 
-    <section class="corp">
-        <form method="post" action="question.php">
-            <h1><p>Modifier une question :</p></h1>
-            <div>
-            <label><p>No Question :</p></label><input type="number", name="numque" max="10" min="1" size="1em" required="required"/>
-            </div>
-            <label><p>Question :</p></label><input type="text" name="question" required="required"/>
-            <p>Consigne</p>
-            <textarea name="Consigne" rows="5" cols="80"  required="required" aria-required="true" style="height: 30%; width: 100%;" ></textarea>
-            <label><p>Réponse :</p></label><input type="text" name="réponse" required="required"/>
-            <label><p>Indice :</p></label><input type="text" name="Indice" required="required"/>
-            <br/>
-            <div><input type="submit" value="Valider"></div>
-        </form>
-        <section class="LeaderBord">
-            <h1><p>LeaderBoard</p></h1>
-            <div class="Player">
-                <?php 
-                echo(classement());
-                ?>
-            </div>
-        </section>
+                    //on affichage le classement
+                    while($row = $sth->fetch()){
+                        echo($row["nickname"]);
+                        echo(" : ");
+                        echo($row["score"]);
+                        echo("<br/>");
+                        echo("<br/>");
+                        echo("<br/>");
+                    }
+            ?>
+        </div>
     </section>
-    <footer>
-        ben le footer
-    </footer>
+</section>
+<footer>
+    ben le footer
+</footer>
 </body>
 </html>
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SITE_SAE_S3
 Site pour l'administration de l'applicaton : "Find The Breach"
+
+A modifier dans les fichiers .php les varible :
+
+$servername -> nom du serv </br>
+$username  -> nom de l'user </br>
+$password -> mdp </br> 
+$db -> nom de la bd </br>
