eab1c18b13ac0f41c897caf116bebe3659956c0c
Saadi Nils <91779594+18Nilsou@users.noreply.github.com>
2022-12-03 17:45:45 +0100

    Add files via upload

--- PHP/pageAdmin.php
+++ PHP/pageAdmin.php
@@ -1,16 +1,37 @@
 <?php 
-require ("bdcon.php");// on require la page pour ce connecter a la bd 
+
+function afficherClassement(){
+    require ("bdcon.php");// on require la page pour ce connecter a la bd
+    $classement = "<br /><br /><br /><br />";
+
+                    
+    $sqlClassement = "Select nickname, score FROM USERS where score is not null order by score DESC limit 5";// requete pour recuperer le classement 
+    $sth = $con->prepare($sqlClassement);
+    $sth->execute();
+
+
+    //on affichage le classement
+    while($row = $sth->fetch()){
+        $classement = $classement.$row["nickname"]." : ".$row["score"]."<br /><br /><br /><br />";
+    }
+    return $classement;
+
+}
+
 
 ?>
 
 <!doctype html>
-<html lang="fr">
+<html lang="en">
+
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Page Admin</title>
-<link rel="stylesheet" href="../CSS/StylePageAdmin.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../CSS/StyleIndex.css">
+  <link rel="stylesheet" href="../CSS/Style2.css">
+  <script src="https://kit.fontawesome.com/a076d05399.js"> </script>
+  <script src="../JS/index.js" defer></script>
+  <title>FindTheBreach</title>
 </head>
 
 <body>
@@ -19,7 +40,7 @@ require ("bdcon.php");// on require la page pour ce connecter a la bd
 
 <header>
     <a href="../index.html">
-        <img class="Logo" src="../IMAGE/FindTheReach.png">
+        <img class="logo" src="../IMAGE/FindTheReach.png">
     </a>
 
     <h1 class="Link">
@@ -37,7 +58,7 @@ require ("bdcon.php");// on require la page pour ce connecter a la bd
         </div>
         <label><p>Question :</p></label><input type="text" name="question" required="required"/>
         <p>Consigne</p>
-        <textarea name="Consigne" rows="5" cols="80"  required="required" aria-required="true" style="height: 30%; width: 100%;" ></textarea>
+        <textarea name="Consigne" rows="5" cols="80"  required="required" aria-required="true" style="height: 30%; width: 80%;" ></textarea>
         <label><p>Réponse :</p></label><input type="text" name="réponse" required="required"/>
         <label><p>Indice :</p></label><input type="text" name="Indice" required="required"/>
         <br/>
@@ -47,29 +68,10 @@ require ("bdcon.php");// on require la page pour ce connecter a la bd
         <h1><p>LeaderBoard</p></h1>
         <div class="Player">
             <?php 
-            echo("<br />");
-            echo("<br />");
-            echo("<br />");
-                    
-                    $sqlClassement = "Select nickname, score FROM USERS where score is not null order by score DESC limit 5";// requete pour recuperer le classement 
-                    $sth = $con->prepare($sqlClassement);
-                    $sth->execute();
-
-                    //on affichage le classement
-                    while($row = $sth->fetch()){
-                        echo($row["nickname"]);
-                        echo(" : ");
-                        echo($row["score"]);
-                        echo("<br/>");
-                        echo("<br/>");
-                        echo("<br/>");
-                    }
+            echo(afficherClassement());
             ?>
         </div>
     </section>
 </section>
-<footer>
-    ben le footer
-</footer>
 </body>
 </html>
