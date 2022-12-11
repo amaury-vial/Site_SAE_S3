76d993262506989326fe0320d1c26c925ee7566c
18Nilsou <nils.saadi@etu.univ-amu.fr>
2022-12-11 15:07:15 +0100

    W3C validator html pas fini

--- PAGES/motDePasseOublie.php
+++ PAGES/motDePasseOublie.php
@@ -1,9 +1,10 @@
-<?php
+<!--HTML W3C fait-->
 
+<?php
 session_start();
 ?>
-<html lang="en">
-
+<!DOCTYPE html>
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
