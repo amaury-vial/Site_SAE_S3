10812cb6bbf705319fd9ff0f890c4b5916118dc9
Saadi Nils <91779594+18Nilsou@users.noreply.github.com>
2022-12-12 16:04:44 +0100

    secu coucou lenny

--- PHP/register.php
+++ PHP/register.php
@@ -9,28 +9,31 @@
     $password = hash("sha256", $_POST["password"].$pseudo);
 
     //requete pour recuperer les user et leurs infos
-    $sqlCheckUser = "SELECT EMAIL, NICKNAME, ID_USER FROM USERS";
+    $sqlCheckUser = "SELECT EMAIL, NICKNAME, ID_USER FROM USERS WHERE nickname = :nom";
     $sth = $con->prepare($sqlCheckUser);
+    $sth -> bindValue(':nom', $pseudo, PDO::PARAM_STR);
     $sth -> execute();
-
+    $row = $sth -> fetch();
     $check = true; //pour vérifier qu'il n'exister pas deja
-    $id = 0;
-
-    while($row = $sth -> fetch()) {    //on check si le user existe deja
-        if ($pseudo == $row['nickname'] || $adr == $row['email'] ){
-            $check = false;//si il y a le pseudo ou l'email existe deja on check a false
-        }
-
-        if($id < $row['id_user']){// on recuper l'id max
-            $id = $row['id_user'];
-        }
+    
+    if ( $row && ($pseudo == $row['nickname'] || $adr == $row['email'])){
+        $check = false;//si il y a le pseudo ou l'email existe deja on check a false
     }
 
-    ++$id;
+    $sql = "SELECT MAX(ID_USER) FROM USERS";
+    $sth = $con->prepare($sql);
+    $sth -> execute();
+    $row = $sth -> fetch();
+    $id = $row['max'] + 1;
+
     if($check){
         $sqlNewUser = "INSERT INTO USERS (ID_USER, EMAIL, NICKNAME, PASSWORD)
-            VALUES ($id,'$adr', '$pseudo' ,'$password')";//on ajoute l'user dans la BD
+            VALUES (:id,':adr', ':pseudo' ,':password')";//on ajoute l'user dans la BD
         $sth = $con->prepare($sqlNewUser);
+        $sth -> bindValue(':adr', $adr, PDO::PARAM_STR);
+        $sth -> bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+        $sth -> bindValue(':password', $password, PDO::PARAM_STR);
+        $sth -> bindValue(':id', $id, PDO::PARAM_INT);
         $sth -> execute();
         header("location: ../index.php");//redirection vers la page pour telecharger le jeux
         exit;
