73056b5c2f148f480b071583d3eb9d47688a4eda
Saadi Nils <91779594+18Nilsou@users.noreply.github.com>
2023-01-12 19:22:30 +0100

    css footer

--- footer-header/footer.php
+++ footer-header/footer.php
@@ -1,11 +1,10 @@
+<link rel="stylesheet" href="../css/footer.css">
 <footer>
-
     <p class="credits">Find the breach © 2022-2023</p>
 
-    <a href="../footer-header/mentions_legales.txt"><p classe="mention">Mentions légales</p></a>
+    <a class="mention" href="../footer-header/mentions_legales.txt"><p>Mentions légales</p></a>
 
     <div class="footer_contact">
         <a class="mailto" href="mailto:findthebreach.noreply@gmail.com">Nous contacter</a>
     </div>
-
 </footer>
